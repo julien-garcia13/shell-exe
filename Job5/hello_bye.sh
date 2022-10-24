5f17e21b716c79379e74317c8bcfa8d6698b4e98
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-10-24 13:06:12 +0200

    Hello & Bye
    Arguments qui affichent "Bonjour..." ou "Au revoir..." selon l'argument demandé.

---
+++ Job5/hello_bye.sh
@@ -0,0 +1,8 @@
+if [ $1 = Hello ] # L'argument est "Hello"
+then
+    echo 'Bonjour, je suis un script!' # Affiche "Bonjour, je suis un script!"
+fi
+if [ $1 = Bye ] # L'argument est "Bye"
+then
+    echo 'Au revoir et bonne journée' # Affiche "Au revoir et bonne journée"
+fi
