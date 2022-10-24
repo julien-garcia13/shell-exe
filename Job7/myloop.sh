36af5f64f076e3e5e4b1f4478cd48355929d2987
julien-garcia13 <91736468+julien-garcia13@users.noreply.github.com>
2022-10-24 14:13:24 +0200

    Boucle
    Il est désormais temps d’entrevoir le monde des boucles. Pour cela, vous allez créer un
    script nommé myloop.sh qui va afficher 10 fois la phrase suivante et afficher en fin de
    phrase un chiffre qui s’incrémente à chaque fois :
    “Je suis un script qui arrive à faire une boucle 1”
    “Je suis un script qui arrive à faire une boucle 2”
    “Je suis un script qui arrive à faire une boucle 3”
    “Je suis un script qui arrive à faire une boucle 4”
    ……..
    “Je suis un script qui arrive à faire une boucle 10”
    Vous devez obligatoirement utiliser les boucles pour réaliser ce script.

---
+++ Job7/myloop.sh
@@ -0,0 +1,4 @@
+for chiffres in 1 2 3 4 5 6 7 8 9 10 # On boucle sur les chiffres de 1 à 10.
+do # On commence la boucle.
+echo "je suis un script qui arrive a faire une boucle $chiffres" # On affiche le message.
+done # On termine la boucle.
